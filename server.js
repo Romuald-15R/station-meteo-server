fe5ddd76cdcf9eb3c66ec17c2736c18430bcd962
romuald1234 <romualdrandriantsitohaina@gmail.com>
2026-03-16 08:18:42 +0300

    node server

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const WebSocket = require("ws");
+const http = require("http");
+
+const app = express();
+app.use(express.json());
+
+// Server HTTP + WebSocket
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+console.log("Serveur Node.js démarré");
+
+// Stockage clients Qt
+let clients = [];
+
+wss.on("connection", ws => {
+    console.log("Client Qt connecté");
+    clients.push(ws);
+
+    ws.on("close", () => {
+        clients = clients.filter(c => c !== ws);
+        console.log("Client Qt déconnecté");
+    });
+});
+
+// Route handraisana data avy amin'ny Wokwi
+app.post("/data", (req, res) => {
+    const temp = req.body.temperature;
+    const hum = req.body.humidity;
+
+    const data = {
+        temperature: temp,
+        humidity: hum,
+        etatTemp: temp < 18 ? "FROID" : temp <= 30 ? "NORMAL" : "CHAUD",
+        etatHum: hum < 40 ? "BASSE" : hum <= 70 ? "NORMALE" : "ELEVEE",
+        time: new Date().toLocaleTimeString()
+    };
+
+    console.log("Data Wokwi:", data);
+
+    // Alefa amin'ny Qt clients
+    clients.forEach(client => client.send(JSON.stringify(data)));
+
+    res.send("OK");
+});
+
+server.listen(3000, () => console.log("Server HTTP + WebSocket écoutant sur port 3000"));
